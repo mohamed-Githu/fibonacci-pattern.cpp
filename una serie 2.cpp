ad8cba6c125324bf4e38faa306b39d3f18e3a8e3
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-02-22 22:28:00 +0200

    Initial commit

---
+++ una serie 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	//Dichiarazione
+	int num,x;
+	num=2;
+	x=0;
+	cout<<1<<endl;
+	cout<<1<<endl;
+	//Processo
+	for (num;num<200;num++)
+	{
+	cout<<num<<endl;
+	num=num+x;	
+	x=x+1;	
+		
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
